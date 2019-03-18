8f4137c4f3442aaf265e56483162e3998d250af0
myselfjivan <jivanghadage@gmail.com>
2019-03-18 15:11:14 +0530

    add error messages

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const keymanager = require('./lib/keymanager');
 const errors = require('./utility/errors');
 const validatorErrors = require('./utility/validatorerrors');
+const errorMessages = require('./utility/errormessages');
 
 module.exports = {
     keymanager,
     errors,
-    validatorErrors
+    validatorErrors,
+    errorMessages
 };
