2efe28bc2c02c186466744484ab2ca87cb74f036
myselfjivan <jivanghadage@gmail.com>
2019-03-18 14:22:05 +0530

    add error validator

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const keymanager = require('./lib/keymanager');
 const errors = require('./utility/errors');
+const validateErrors = require('./utility/validaterrors');
 
 module.exports = {
     keymanager,
     errors,
+    validateErrors
 };
