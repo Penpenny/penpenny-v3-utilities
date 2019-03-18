60f8eac3f9f6200086bf642ab5633956ddcd82a4
myselfjivan <jivanghadage@gmail.com>
2019-03-18 16:52:09 +0530

    update index

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const lib = require('./lib');
-const utility = require('./utility');
+const acl = require('./utility/acl');
+const errors = require('./utility/errors');
+const validatorErrors = require('./utility/validatorerrors');
+const errorMessages = require('./utility/errormessages');
+const notifications = require('./lib/notifications');
+const keymanager = require('./lib/keymanager');
+
 
 module.exports = {
-    lib,
-    utility,
+    acl,
+    errors,
+    validatorErrors,
+    errorMessages,
+    keymanager,
+    notifications,
 };
--- utility/index.js
+++
@@ -1,9 +0,0 @@
-const errors = require('./errors');
-const validatorErrors = require('./validatorerrors');
-const errorMessages = require('./errormessages');
-
-module.exports = {
-    errors,
-    validatorErrors,
-    errorMessages,
-};
